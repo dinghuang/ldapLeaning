89c617b507403ab1cbfde8fde1aa75d6a87d37f0
dinghuang <dinghuang123@gmail.com>
2018-06-10 16:21:25 +0800

    [ADD] init project

---
+++ src/main/java/ldaptest/DemoApplication.java
@@ -0,0 +1,25 @@
+package ldaptest;
+
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+//    @Bean
+//    public static PropertySourcesPlaceholderConfigurer properties() {
+//        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+//        YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
+//        yaml.setResources(new ClassPathResource("application-default.yml"));
+//        propertySourcesPlaceholderConfigurer.setProperties(yaml.getObject());
+//        return propertySourcesPlaceholderConfigurer;
+//    }
+}
